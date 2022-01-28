c6ec4208e3828d3d65ab96b44c561a2864fdf4f5
crypto-ninja <kotovubalenko.ku@gmail.com>
2022-01-28 17:57:04 +0900

    feat: add header component

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,14 @@
 import headerStyles from '../styles/Header.module.css'
 
 const Header = () => {
-    const x = 2;
     return (
         <div>
-            <h1><span className='title'>WebDev</span>News</h1>
-            <style jsx>
-                {`
-                    .title {
-                        color: ${x > 3 ? 'red' : 'blue'};
-                    }
-                `}
-            </style>
+            <h1 className={headerStyles.title}>
+                <span>WebDev</span> News
+            </h1>
+            <p className={headerStyles.description}>
+                Keep up to date with the latest web dev news
+            </p>
         </div>
         );
 };
