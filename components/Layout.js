a56b37fec369e53cd7c188d8a3dc5f6da784f569
crypto-ninja <kotovubalenko.ku@gmail.com>
2022-01-28 17:31:22 +0900

    feat: add Nav component

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,17 @@
 import styles from '../styles/Layout.module.css'
+import Nav from './Nav'
 
 const Layout = ({ children }) => {
     return (
-        <div className={styles.container}>
-            <main className={styles.main}>
-                {children}
-            </main>
-        </div>
+        <>
+            <Nav></Nav>
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    {children}
+                </main>
+            </div>
+        </>
+        
     )
 }
 
