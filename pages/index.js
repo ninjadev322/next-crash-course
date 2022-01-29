188daafa7727dbb048406f65757d43eee621ac6d
crypto-ninja <kotovubalenko.ku@gmail.com>
2022-01-29 20:21:53 +0900

    feat: api added

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-/* eslint-disable react/jsx-key */
+import {server} from '../config'
 import Head from 'next/head'
 import ArticleList from '../components/ArticleList'
 
@@ -18,7 +18,7 @@ export default function Home({ articles }) {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json();
 
   return {
@@ -27,3 +27,14 @@ export const getStaticProps = async () => {
     }
   }
 }
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json();
+
+//   return {
+//     props: {
+//       articles
+//     }
+//   }
+// }
